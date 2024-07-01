ece25d78dc8ec0f9159e14704b5d837ab219d5dd
feririfki <feririfki19@gmail.com>
2024-07-01 16:13:05 +0700

    tolpemesanan

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -80,7 +80,7 @@
                 <li class="nav-item">
                     <a href="/order" class="nav-link {{ request()->is('order','order/*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-layer-group"></i>
-                        <p>TOOl ORDER</p>
+                        <p>TOOL PEMESANAN</p>
                     </a>
                 </li>
                 @endif
